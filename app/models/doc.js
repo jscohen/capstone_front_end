b9890e8ad4e298ca70151763d39f9645e8993a21
jscohen <jon.cohen08@gmail.com>
2017-06-07 15:02:41 -0400

    Added dist for deployment

--- app/models/doc.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  text: DS.attr('string'),
-  title: DS.attr('string'),
-  _id: DS.attr('string')
-});
--- app/routes/new-document.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  actions: {
-    newDoc () {
-      this.get('flashMessages').success('Document Created')
-    }
-  }
-});
